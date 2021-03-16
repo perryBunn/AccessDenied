33b4e5825c6fcc6cf711304feb9b7bf97a0c65a4
Perry Bunn <eagle4ever20@gmail.com>
2021-03-15 22:00:37 -0500

    cleaning branch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,38 +7,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private playerinput input;
-
-    private Vector2 currentMove;
-    private bool movePressed;
-    private bool runPressed;
-
-    private void Awake()
-    {
-        input = new playerinput();
-
-        input.Player.Move.performed += ctx =>
-        {
-            currentMove = ctx.ReadValue<Vector2>();
-            movePressed = currentMove.x != 0 || currentMove.y != 0;
-        };
-        input.Player.Move.performed += ctx => Debug.Log(ctx.ReadValueAsObject());
-    }
+    private Vector3 moveVec;
+    private bool jumping = false;
+    
 
     private void Start()
     {
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        handleMovement();
+        // 1
+        float speedX = Input.GetAxisRaw("Horizontal");  // Left, Right
+        float speedY = Input.GetAxisRaw("Vertical");  // Back, Forward
+        moveVec = new Vector3(speedX, 0, speedY);
     }
 
-    void handleMovement()
+    public void OnMove(InputValue input)
     {
-        if (movePressed)
-        {
-            Debug.Log("Movement");
-        }
+        Vector2 inputVec = input.Get<Vector2>();
+
+        moveVec = new Vector3(inputVec.x, 0, inputVec.y);
     }
 
     private void OnEnable()
